d9a34cc4e883f42997be261075563c629425820b
yellowclub-by <hello@yellowclub.by>
2025-05-03 19:44:06 +0300

    add magic filters

--- handlers/user_command.py
+++ handlers/user_command.py
@@ -1,4 +1,4 @@
-from aiogram import types, Router
+from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command
 
 user_routor=Router()
@@ -7,32 +7,41 @@ user_routor=Router()
 async def start_cmd(message:types.Message):
     await message.answer("Это бот для РПГ Персонажей. Введите ваше имя?")
     user_text=message.text
-@user_routor.message(Command('charecter'))
+@user_routor.message(Command('charecter'), F.text.contains("Персонаж"))
 async def Charecter(message:types.Message):
     await message.answer('Ваш персонаж')
-@user_routor.message(Command('backpack'))
+@user_routor.message(Command('backpack'), F.text.contains("Рюкзак"))
 async def backpack(message:types.Message):
     await message.answer('У вас в рюкзаке')
-@user_routor.message(Command('abilite'))
+@user_routor.message(Command('abilite'), F.text.contains("Навыки"))
 async def abilite(message:types.Message):
     await message.answer('Вы можете делать')
-@user_routor.message(Command('equipment'))
+@user_routor.message(Command('equipment'), F.text.contains("Снаряжение"))
 async def equipment(message:types.Message):
     await message.answer('На вас надето')
-@user_routor.message(Command('name'))
+@user_routor.message(F.text.lower().contains("имя"))
+async def echo(message: types.Message):
+    user_text = message.text
+    name = user_text
+    await message.answer(f'Ваше имя {name}')
+@user_routor.message(Command('name'), F.text.contains("Имя"))
 async def your_name(message:types.Message):
     await message.answer('Введите имя')
-    # name = name1()
-    # await message.answer(f'Ваше имя {name}')
 # def name1(message:types.Message):
 #     name=message.text
 #     return name
-@user_routor.message(Command('age'))
+@user_routor.message(Command('age'), F.text.contains("Возрост"))
 async def age(message:types.Message):
     pass
-# @user_routor.message()
+# @user_routor.message(F.text)
 # async def echo(message:types.Message):
 #     user_text=message.text
 #     name=user_text
-#     return name
+# #     return name
 #     await message.answer(f"Это ваше имя {name}")
+# @user_routor.message(F.text=="почему")
+# @user_routor.message(F.photo)
+# @user_routor.message(F.text.lower()=="почему")
+# @user_routor.message(F.text.lower().contains("почему"))
+# @user_routor.message(F.text.lower().startswith("почему"))
+
